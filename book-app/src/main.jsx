f29d9d61e27351e5eae32b02a33352901eea36b6
hpknollenberg <hpknollenberg@gmail.com>
2024-08-30 16:58:39 -0400

    called google books api

--- book-app/src/main.jsx
+++ book-app/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//project styles
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App.jsx'
+
+function Layout() {
+  return (
+    <>
+        <div id='page-content'>
+          <Outlet />
+        </div>
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+    ]
+  }
+])
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
